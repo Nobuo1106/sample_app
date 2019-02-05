83cf134f05fa8eba83458813267de4ace65202a7
EC2 Default User <ec2-user@ip-172-31-38-75.us-east-2.compute.internal>
2019-02-05 07:30:39 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  # => contact_path
   get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'application#hello'
-
+  post '/signup', to: 'users#create'
+  resources :users
 end
