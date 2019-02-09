a9bfc31976b49bb3b98c4565311a3f2ad1b2e804
EC2 Default User <ec2-user@ip-172-31-38-75.us-east-2.compute.internal>
2019-02-09 08:07:27 +0000

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
   
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+  
    test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
@@ -25,4 +35,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+    
+    test "login with remembering" do
+      log_in_as(@user, remember_me:'1')
+      assert_equal FILL_IN, assigns(:user).FILL_IN
+    end
+    
+    test "login without remembering" do
+      log_in_as(@user, remember_me:'1')
+      delete logout_path
+      log_in_as(@user, remember_me:'0')
+      asserrt_empty cookies['remember_token']
+    end    
 end
