0909b0d0d61df63c9b7260f527aaed0e8dec93f2
EC2 Default User <ec2-user@ip-172-31-38-75.us-east-2.compute.internal>
2019-02-12 03:42:44 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,11 @@
 module SessionsHelper
-  
+
   def log_in(user)
     session[:user_id] = user.id
   end
-  
+
   def remember(user)
-    user.remember
+    user.remember # => DB: remember_digest
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
@@ -16,10 +16,16 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: [:user_id])
+      @current_user ||= User.find_by(id: user_id)
+
     elsif (user_id = cookies.signed[:user_id])
+      # raise
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -36,5 +42,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
